135ca37c3194e578875634230be6b83e3de3bdb8
robomatix <robomatix@robomatix.net>
2015-01-31 14:50:28 +0100

    60 bouncings squares !!!!

--- js/game/states/Game.js
+++ js/game/states/Game.js
@@ -26,6 +26,19 @@ btbs.Game.prototype = {
         this.player.body.collideWorldBounds = true;
         this.player.body.bounce.set(0.42);
 
+        squares = this.game.add.group();
+        squares.enableBody = true;
+
+        for (var i = 0; i < 60; i++)
+        {
+            var s = squares.create(this.game.world.randomX, this.game.world.randomY, 'square');
+            s.name = 'square' + s;
+            s.tint = 0x000000;
+            s.body.collideWorldBounds = true;
+            s.body.bounce.setTo(0.8, 0.8);
+            s.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
+        }
+
 
 
 
