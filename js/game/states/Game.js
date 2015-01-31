17f7b2881c0dc1feabe47230c22c19562b06e641
robomatix <robomatix@robomatix.net>
2015-01-31 15:06:12 +0100

    destroying the squares group on shutdown !

--- js/game/states/Game.js
+++ js/game/states/Game.js
@@ -19,24 +19,26 @@ btbs.Game.prototype = {
         this.player.smoothed = false;
 
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        //this.game.physics.arcade.gravity.y = 400;
+
 
 
         this.game.physics.arcade.enableBody(this.player);
         this.player.body.collideWorldBounds = true;
-        this.player.body.bounce.set(0.42);
 
-        squares = this.game.add.group();
-        squares.enableBody = true;
+
+        this.squares = this.game.add.group();
+        this.squares.enableBody = true;
 
         for (var i = 0; i < 60; i++)
         {
-            var s = squares.create(this.game.world.randomX, this.game.world.randomY, 'square');
+
+            var s = this.squares.create(this.game.world.randomX, this.game.world.randomY, 'square');
             s.name = 'square' + s;
             s.tint = 0x000000;
             s.body.collideWorldBounds = true;
             s.body.bounce.setTo(0.8, 0.8);
             s.body.velocity.setTo(10 + Math.random() * 40, 10 + Math.random() * 40);
+
         }
 
 
@@ -47,13 +49,16 @@ btbs.Game.prototype = {
 
         this.player.rotation = this.game.physics.arcade.moveToPointer(this.player, 120, this.game.input.activePointer, 350);
 
+        this.game.physics.arcade.collide(this.player, this.squares);
+
         //this.game.physics.arcade.overlap(this.player, this.coins, this.coinHit, null, this);
-        //this.game.physics.arcade.overlap(this.player, this.enemies, this.enemyHit, null, this);
+
 
     },
     shutdown: function () {
 
         this.player.destroy();
+        this.squares.destroy();
 
         this.score = 0;
 
